2ae8b54b58802262b0d4fcf5b6efdb3915da2f08
Chiichriis <chichris28@gmail.com>
2023-06-27 08:23:28 +0100

    updt1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import Header from './components/Header'
+import Home from './components/Home'
+import Notification from './components/Notification'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <NavLink to="/" >Home</NavLink>
+          <NavLink to="notification">Notify</NavLink>
+        </nav>
+      </header>
+      <main className='bg-red-100'>
+        <Routes>
+          <Route path='/' element ={Home}/>
+          <Route path='/notification' element ={Notification} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi voluptatum doloremque ea natus animi nostrum sequi! Architecto esse corrupti molestiae similique voluptatibus, illo deleniti minima at, nulla quod, quae enim.
+    </div>
+  )
+}
+
+export default Home
