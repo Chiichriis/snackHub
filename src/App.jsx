2543e289a9b0bf4ad2eb017fd5d3914360fb09d4
chiichriis <chichris28@gmail.com>
2023-07-11 17:40:23 +0100

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Notification from './components/Notification'
 import Search from './components/Search'
 import Profile from './Profile'
+import homeIcon from './components/imgs/home.png'
 import notifyIcon from './components/imgs/notification-icon.png'
 import searchIcon from './components/imgs/search-icon.png'
 import profileIcon from './components/imgs/profile.jpg'
@@ -14,13 +15,16 @@ const App = () => {
   return (
     <BrowserRouter>
       <header 
-        className = 'p-1 px-6 py-12 md:py-16 md:px-32 fixed w-full h-20 md:h-24 flex items-center justify-between shadow-sm bg-red-100'>
+        className = 'p-1 px-6 py-16 md:px-32 fixed w-full h-20 md:h-24 flex items-center justify-between shadow-md'>
         <div>
-          <p>Snack Hub.</p>
+          <p className='font-logo text-xl text-slate-600'>SnackHub.</p>
         </div>
 
         <nav 
           className='flex gap-5 rounded-3xl border px-3 py-4 items-center '>
+          <NavLink to="/" >
+            <img src={homeIcon} alt="home" className='h-7 w-auto hd:h-10'/>
+          </NavLink>
           <NavLink to="search" >
             <img src={searchIcon} alt="search" className='h-7 md:h-10 w-auto'/>
           </NavLink>
@@ -28,7 +32,7 @@ const App = () => {
             <img src={notifyIcon} alt="notifications" className='h-7 md:h-10 w-auto'/>
           </NavLink>
           <NavLink to="profile">
-            <img src={profileIcon} alt="profile" className='h-10 md:h-16 w-auto border rounded-full border-black ml-10' />
+            <img src={profileIcon} alt="profile" className='h-10 md:h-16 w-auto border rounded-full border-black ml-3 md:ml-7' />
           </NavLink>
         </nav>
       </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: 'Chela One',
+        primary: 'Noto Sans',
+      }
+    },
   },
   plugins: [],
 }
